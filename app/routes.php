ac5ef28eff5aafe241235964be4862f2ccce08a2
event <event@e2r1p3.42.fr>
2014-12-13 22:59:00 +0100

    correction bug

--- app/routes.php
+++ app/routes.php
@@ -10,12 +10,7 @@
 | and give it the Closure to execute when that URI is requested.
 |
 */
-require_once 'views/thingiverse.php'; 
 Route::get('/', function()
 {
-	$thingiverse = new Thingiverse();
-	if (!isset($_GET["code"]))
-		return Redirect::to($thingiverse->makeLoginURL());
-	else
-		return View::make('hello');
+	return View::make('hello');
 });
