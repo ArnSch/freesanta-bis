d0ada5863ee0b6086fd6d910ad77212a19286aca
event <event@e2r1p3.42.fr>
2014-12-14 00:57:24 +0100

    click -> clone model 3d

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -1,10 +1,27 @@
 $(document).ready(function() {
-  $("#owl-demo").owlCarousel({
-    navigation: true,
-    items: 2,
-    navigationText: [
-      "<i class='fa fa-chevron-circle-left fa-3x'></i>",
-      "<i class='fa fa-chevron-circle-right fa-3x'></i>"
-      ]
-  });
+	$("#owl-demo").owlCarousel({
+	navigation: true,
+	items: 2,
+	navigationText: [
+			"<i class='fa fa-chevron-circle-left fa-3x'></i>",
+			"<i class='fa fa-chevron-circle-right fa-3x'></i>"
+		]
+	});
+	$(".item").each(function(index, el) {
+		var url = $(this).attr('src');
+		jQuery.getScript(url);
+	});
+});
+
+$("body").on('click', ".item", function(event){
+	console.log("click");
+	var clone = $(this).clone();
+	$(".selectedModelContainer").each(function(index, el) {
+		console.log($(this).html());
+		if ($(this).html() === ""){
+			console.log("clone");
+			$(this).append(clone);
+			return ;
+		}
+	});
 });
