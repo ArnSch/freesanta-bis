bc48ba380a09ccbd258a31f99ff42d8ed7561f54
event <event@e2r1p3.42.fr>
2014-12-13 22:52:01 +0100

    carousel

---
+++ public/js/carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("#owl-demo").owlCarousel({
+    navigation: true,
+    items: 2,
+    navigationText: [
+      "<i class='fa fa-chevron-circle-left fa-3x'></i>",
+      "<i class='fa fa-chevron-circle-right fa-3x'></i>"
+      ]
+  });
+});
